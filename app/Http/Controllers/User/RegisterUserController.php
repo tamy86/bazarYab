029ea1d937799db67806e60e3538fe43392473a9
tamy86 <34414031+tamy86@users.noreply.github.com>
2021-08-10 04:29:25 -0700

    creat sp UsersCheckUserPhoneExistsInBusinessCustomers to check user is customer or not and return info (name,family) of users at login in first step checking user inserted at customerusers table or not if not checking is customer of any business user or not if is customer sent a verify code to user and instert him into table if not show msg and login completed for customer users in this commit and push      19/05/1400 ->commit no=38

--- app/Http/Controllers/User/RegisterUserController.php
+++ app/Http/Controllers/User/RegisterUserController.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use League\Flysystem\Config;
+use Illuminate\Support\Facades\DB;
 
 class RegisterUserController extends Controller
 {
@@ -27,66 +28,85 @@ class RegisterUserController extends Controller
         try {
 
             $phoneNo = $request->input('phone');
-            $verifyCode = rand(10000, 99999);
-            $ipBusiness = $request->ip();
-
-            /**check phone exists and roleid is new or exits if new insert else update**/
-            $phoneexist = Customeruser::where('phone', $phoneNo)->exists();
-
-            if ($phoneexist === false) {
-                //insert to DB
-                $userBusinessRegister = new Customeruser();
-                $userBusinessRegister->verify = bcrypt($verifyCode);
-                $userBusinessRegister->signin = 0;
-                $userBusinessRegister->ipaddress = $ipBusiness;
-                $userBusinessRegister->created_at = new \DateTime();
-                $userBusinessRegister->updated_at = new \DateTime();
-                $userBusinessRegister->phone = $phoneNo;
-                $userBusinessRegister->save();
-
-                return response()->json([
-                    'Success' => 1,
-                    'message' => 'کد اعتبار سنجی به شماره همراه شما ارسال شد',
-                    'verify' => $verifyCode,
-                    'message type' => 'success'
-                ], 201);
-
-            } else
-                if ($phoneexist === true) {
-//            $updatedate = Businessuser::select('updated_at')->where('phone', $phoneNo)->first()->updated_at;
-                    $businessData = Customeruser::select('updated_at')->where('phone', $phoneNo)->get();
-
-                    foreach ($businessData as $data) {
 
 
-                        $updatedate = $data->updated_at;
+                $verifyCode = rand(10000, 99999);
+                $ipBusiness = $request->ip();
 
-                    }
+                /**check phone exists and roleid is new or exits if new insert else update**/
+                $phoneexist = Customeruser::where('phone', $phoneNo)->exists();
 
-                    $now = Carbon::now();
+                if ($phoneexist === false) {
 
-                    $differentMin = $updatedate->diffInMinutes($now);
+                    $checkUserPhoneExists=DB::select('CALL UsersCheckUserPhoneExistsInBusinessCustomers(?)',array($phoneNo));
+                    $countUserPhoneExists=$checkUserPhoneExists[0]->countphone;
+                    $userName=$checkUserPhoneExists[0]->name;
+                    $userFamily=$checkUserPhoneExists[0]->family;
 
-                    if ($differentMin > 3) {
+                     if($countUserPhoneExists>0) {
+                         //insert to DB
+                     $userBusinessRegister = new Customeruser();
+                     $userBusinessRegister->verify = bcrypt($verifyCode);
+                     $userBusinessRegister->name=$userName;
+                     $userBusinessRegister->family=$userFamily;
+                     $userBusinessRegister->signin = 0;
+                     $userBusinessRegister->status = 0;
+                     $userBusinessRegister->ipaddress = $ipBusiness;
+                     $userBusinessRegister->created_at = new \DateTime();
+                     $userBusinessRegister->updated_at = new \DateTime();
+                     $userBusinessRegister->phone = $phoneNo;
+                     $userBusinessRegister->save();
 
-                        Customeruser::where('phone', $phoneNo)->update(['verify' => bcrypt($verifyCode)]);
-
-                        return response()->json([
-                            'Success' => 1,
-                            'message' => 'کد اعتبار سنجی مجددا به شماره همراه شما ارسال شد',
-                            'verify' => $verifyCode,
-                            'message type' => 'success',
-
-                        ]);
-
-                    } else {
+                    return response()->json([
+                        'Success' => 1,
+                        'message' => 'کد اعتبار سنجی به شماره همراه شما ارسال شد',
+                        'verify' => $verifyCode,
+                        'exist' => $countUserPhoneExists,
+                        'message type' => 'success'
+                    ], 201);
+                    }else{
                         return response()->json([
                             'Success' => 0,
-                            'message' => 'به دلیل درخواست متعدد پس از 3 دقیقه مجدد تلاش نمایید',
+                            'message' => 'شما تاکنون جزو مشتریان هیچ کسب و کاری نیستید و امکان لاگین ندارید',
                             'message type' => 'warning',
                         ], 429);
                     }
-                }
+                } else
+                    if ($phoneexist === true) {
+//            $updatedate = Businessuser::select('updated_at')->where('phone', $phoneNo)->first()->updated_at;
+                        $businessData = Customeruser::select('updated_at')->where('phone', $phoneNo)->get();
+
+                        foreach ($businessData as $data) {
+
+
+                            $updatedate = $data->updated_at;
+
+                        }
+
+                        $now = Carbon::now();
+
+                        $differentMin = $updatedate->diffInMinutes($now);
+
+                        if ($differentMin > 3) {
+
+                            Customeruser::where('phone', $phoneNo)->update(['verify' => bcrypt($verifyCode)]);
+
+                            return response()->json([
+                                'Success' => 1,
+                                'message' => 'کد اعتبار سنجی مجددا به شماره همراه شما ارسال شد',
+                                'verify' => $verifyCode,
+                                'message type' => 'success',
+
+                            ]);
+
+                        } else {
+                            return response()->json([
+                                'Success' => 0,
+                                'message' => 'به دلیل درخواست متعدد پس از 3 دقیقه مجدد تلاش نمایید',
+                                'message type' => 'warning',
+                            ], 429);
+                        }
+                    }
 
         }catch (\Exception $exception)
         {
@@ -113,10 +133,10 @@ class RegisterUserController extends Controller
 
                 ], 401);
             }
-
+            return $this->createNewToken($token);
             Customeruser::where('phone', $request->input('phone'))->update(['token' => $token]);
 
-            return $this->createNewToken($token);
+
 
         }catch (\Exception $exception){
 
@@ -124,6 +144,7 @@ class RegisterUserController extends Controller
                 return response()->json([
                     'message' => '5202 خطا در ارتباط با سرور یا داده وروودی لطفا با پشتیبانی تماس بگیرید ',
                     'message type' => 'error',
+                    'ex'=>$exception,
                 ], 500);
             }
         }
